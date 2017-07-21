df9c0914b85d0b4bc655272450f79532964b7290
rravanales <rodrigo.ravanales@gmail.com>
2017-07-21 16:46:34 -0400

    paquetes

---
+++ 01 - Packages.R
@@ -0,0 +1,14 @@
+# Chapter 01 - Packages
+
+####  Download and install the packages from CRAN
+ install.packages("xlsx")
+# install.packages("sqldf")
+# install.packages("ggplot2")
+
+####  Load the package into the session
+library(xlsx)
+library(sqldf)
+library(ggplot2)
+
+####  Load own libraries
+#source("myLibrary.R",local = TRUE)
