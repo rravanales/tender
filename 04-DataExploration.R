f4c1d2db0fa54ca8eddd6838ac92e0880c1d8905
Rodrigo Ravanales <rodrigo.ravanales@MacBook-de-Rodrigo.local>
2017-07-24 11:27:20 -0400

    Data Exploration

--- 04-DataExploration.R
+++ 04-DataExploration.R
@@ -5,49 +5,110 @@ format(t(s), big.mark = ".", decimal.mark = ",")
 
 #### Simple graphs ####
 
-# 1 - kilograms daily
+# 1 - daily kilograms
+
 head(dispatches)
 summary(dispatches)
-dis_by_date         <- group_by(dispatches, date)
-dis_by_date_sum_kgs <- summarise(dis_by_date, kilograms = sum(kilograms))
-plt <- qplot(x=date, y=kilograms, data=dis_by_date_sum_kgs, geom="point")
-plt <- plt + labs(title = "kilograms daily", subtitle = "year 2015", caption = "3e+06 = 3.000.000")
-plt
-summary(dis_by_date_sum_kgs)
+daily_dis         <- group_by(dispatches, date)
+daily_dis_sum_kgs <- summarise(daily_dis, kilograms = sum(kilograms))
+summary(daily_dis_sum_kgs)
 
-# 2 - kilograms daily group by trip
-head(base)
-summary(base)
-dis_by_date_trip          <- group_by(base, date, trip)
-dis_by_date_trip_sum_kgs  <- summarise(dis_by_date_trip, kilograms = sum(kilograms))
-plt <- qplot(x=date, y=kilograms, data=dis_by_date_trip_sum_kgs, geom="point", color=trip)
-plt <- plt + labs(title = "kilograms daily group by trip", subtitle = "year 2015", caption = "@rravanales")
-plt
-summary(dis_by_date_trip_sum_kgs)
+# 1.1
+qplot(x    = date, 
+      y    = kilograms, 
+      data = daily_dis_sum_kgs, 
+      geom = "point"
+     ) + labs(title    = "Daily kilograms", 
+              subtitle = "year 2015", 
+              caption  = "3e+06 = 3.000.000")
 
 # outliers
-plt <- qplot(x=date, y=kilograms, data=dis_by_date_trip_sum_kgs, geom=c("smooth","point"), color=trip)
-plt <- plt + labs(title = "kilograms daily group by trip - Smooth, Point ", subtitle = "year 2015", caption = "@rravanales")
-plt
+# 1.2
+qplot(x    = "1",
+      y    = kilograms,
+      data = daily_dis_sum_kgs,
+      geom = "boxplot"
+) + labs(title    = "Daily kilograms",
+         subtitle = "year 2015  - boxplot", 
+         caption  = "Common values, tipical values and outliers")
 
 
-plt <- qplot(x=date, y=kilograms, data=dis_by_date_trip_sum_kgs, geom=c("smooth"), color=trip)
-plt <- plt + labs(title = "kilograms daily group by trip - Smooth", subtitle = "year 2015", caption = "@rravanales")
-plt
+# 2 - daily kilograms group by trip
 
-plt <- qplot(x=trip, y=kilograms, data=dis_by_date_trip_sum_kgs, geom="boxplot")
-plt <- plt + labs(title = "kilograms daily group by trip - Boxplot", subtitle = "year 2015", caption = "@rravanales")
-plt
-
-# 3 - kilograms daily group by trip and service
+head(base)
 summary(base)
-base_f1                           <- filter(base, date >= '2015-12-01')
-dis_daily_by_trip_service         <- group_by(base_f1, date, trip, service)
-dis_daily_by_trip_service_sum_kgs <- summarise(dis_daily_by_trip_service, kilograms = sum(kilograms))
-qplot(x=date, y=kilograms, data=dis_daily_by_trip_service_sum_kgs, geom="smooth", color=trip, shape=service)
+daily_dis_by_trip          <- group_by(base, date, trip)
+daily_dis_by_trip_sum_kgs  <- summarise(daily_dis_by_trip, kilograms = sum(kilograms))
+summary(daily_dis_by_trip_sum_kgs)
+
+# 2.1
+qplot(x     = trip,
+      data  = daily_dis_by_trip_sum_kgs,
+      geom  = "bar",
+      fill = trip
+) + labs(title    = "Number of days worked by trip",
+         subtitle = "year 2015",
+         caption  = "")
+# 2.2
+qplot(x     = date,
+      y     = kilograms, 
+      data  = daily_dis_by_trip_sum_kgs,
+      geom  = "point", 
+      color = trip
+     ) + labs(title    = "Daily kilograms group by trip",
+              subtitle = "year 2015",
+              caption  = "")
+
+# 2.3
+qplot(x    = date,
+      y    = kilograms,
+      data = daily_dis_by_trip_sum_kgs,
+      geom = c("smooth","point"),
+      color = trip
+     ) + labs(title    = "Daily kilograms group by trip",
+              subtitle = "year 2015  - Smooth, Point", 
+              caption  = "")
+
+# 2.4
+qplot(x    = date,
+      y    = kilograms,
+      data = daily_dis_by_trip_sum_kgs,
+      geom = c("smooth"),
+      color = trip
+     ) + labs(title    = "Daily kilograms group by trip",
+              subtitle = "year 2015  - Smooth", 
+              caption  = "")
+
+# outliers
+# 2.5
+qplot(x    = reorder(trip,kilograms),
+      y    = kilograms,
+      data = daily_dis_by_trip_sum_kgs,
+      geom = "boxplot"
+     ) + labs(title    = "Daily kilograms group by trip",
+         subtitle = "year 2015  - boxplot", 
+         caption  = "")
+
+
+
 
 #### Combined graphs ####
 
+# 3 - kilograms daily group by trip and service (map)
 
+summary(base)
+base_f1                     <- filter(base, date >= '2015-12-01')
+dis_by_trip_service         <- group_by(base, trip, service)
+dis_by_trip_service_sum_kgs <- summarise(dis_by_trip_service, kilograms = sum(kilograms))
+summary(base)
 
-dis_by_date_trip_sum_kgs
+# 3.1
+qplot(x    = service, 
+      data = base, 
+      geom = "bar",
+      fill = trip,
+      position = "dodge"
+) + labs(title    = "Number of services worked by trip ", 
+         subtitle = "year 2015", 
+         caption  = "")
+
