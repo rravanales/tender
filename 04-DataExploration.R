56a9767d1530307bf8d3071108609152c2e5a7bc
rravanales <rodrigo.ravanales@gmail.com>
2017-07-27 22:39:19 -0400

    Data Exploration

--- 04-DataExploration.R
+++ 04-DataExploration.R
@@ -3,119 +3,96 @@
 
 # 4.1 Simple graphs -------------------------------------------------------
 
-# 4.1.1 - Daily kilograms -------------------------------------------------
+# 4.1.1 - Daily kilograms demand ------------------------------------------
 
-head(dispatches)
-summary(dispatches)
-daily_dis         <- group_by(dispatches, date)
-daily_dis_sum_kgs <- summarise(daily_dis, kilograms = sum(kilograms))
-summary(daily_dis_sum_kgs)
+head(c.dispatches)
+v.daily_dem    <- group_by(c.dispatches, date)
+s.daily_kg_dem <- summarise(v.daily_dem, daily_kg = sum(kilograms))
+s.daily_kg_dem
 
 # a)
 qplot(x    = date, 
-      y    = kilograms, 
-      data = daily_dis_sum_kgs, 
+      y    = daily_kg, 
+      data = s.daily_kg_dem, 
       geom = "point"
-     ) + labs(title    = "Daily kilograms", 
+     ) + labs(title    = "Daily kilograms demand", 
               subtitle = "year 2015", 
-              caption  = "3e+06 = 3.000.000")
+              caption  = "3e+06 = 3.000.000 | Figure 1.1")
 
-# outliers
-# b)
-qplot(x    = "1",
-      y    = kilograms,
-      data = daily_dis_sum_kgs,
+# b) Outliers
+qplot(x    = " ",
+      y    = daily_kg,
+      data = s.daily_kg_dem,
       geom = "boxplot"
-) + labs(title    = "Daily kilograms",
+) + labs(title    = "Daily kilograms demand",
          subtitle = "year 2015  - boxplot", 
-         caption  = "Common values, tipical values and outliers")
+         caption  = "Common values, tipical values and outliers | Figure 1.2")
+summary(s.daily_kg_dem)
 
-
-# 4.1.2 - Daily kilograms group by trip -----------------------------------
+# 4.1.2 - Daily kilograms demand group by trip ----------------------------
 
 head(base)
-summary(base)
-daily_dis_by_trip          <- group_by(base, date, trip)
-daily_dis_by_trip_sum_kgs  <- summarise(daily_dis_by_trip, kilograms = sum(kilograms))
-summary(daily_dis_by_trip_sum_kgs)
+v.daily_dem_by_trip     <- group_by(base, date, trip)
+s.daily_kg_dem_by_trip  <- summarise(v.daily_dem_by_trip, daily_kg = sum(kilograms))
+s.daily_kg_dem_by_trip
 
 # a)
 qplot(x     = trip,
-      data  = daily_dis_by_trip_sum_kgs,
+      data  = s.daily_kg_dem_by_trip,
       geom  = "bar",
       fill = trip
 ) + labs(title    = "Number of days worked by trip",
          subtitle = "year 2015",
-         caption  = "")
-# b)
+         caption  = "Figure 2.1")
+# b.1)
 qplot(x     = date,
-      y     = kilograms, 
-      data  = daily_dis_by_trip_sum_kgs,
+      y     = daily_kg, 
+      data  = s.daily_kg_dem_by_trip,
       geom  = "point", 
       color = trip
-     ) + labs(title    = "Daily kilograms group by trip",
+     ) + labs(title    = "Daily kilograms demand group by trip",
               subtitle = "year 2015",
-              caption  = "")
+              caption  = "Figure 2.2.a")
 
-# c)
+# b.2)
 qplot(x    = date,
-      y    = kilograms,
-      data = daily_dis_by_trip_sum_kgs,
+      y    = daily_kg,
+      data = s.daily_kg_dem_by_trip,
       geom = c("smooth","point"),
       color = trip
-     ) + labs(title    = "Daily kilograms group by trip",
+     ) + labs(title    = "Daily kilograms demand group by trip",
               subtitle = "year 2015  - Smooth, Point", 
-              caption  = "")
+              caption  = "Figure 2.2.b")
 
-# d)
+# b.3)
 qplot(x    = date,
-      y    = kilograms,
-      data = daily_dis_by_trip_sum_kgs,
+      y    = daily_kg,
+      data = s.daily_kg_dem_by_trip,
       geom = c("smooth"),
       color = trip
-     ) + labs(title    = "Daily kilograms group by trip",
+     ) + labs(title    = "Daily kilograms demand group by trip",
               subtitle = "year 2015  - Smooth", 
-              caption  = "")
+              caption  = "Figure 2.2.c")
 
-# outliers
-# e)
-qplot(x    = reorder(trip,kilograms),
-      y    = kilograms,
-      data = daily_dis_by_trip_sum_kgs,
+# c) Outliers
+qplot(x    = reorder(trip,daily_kg),
+      y    = daily_kg,
+      data = s.daily_kg_dem_by_trip,
       geom = "boxplot"
      ) + labs(title    = "Daily kilograms group by trip",
          subtitle = "year 2015  - boxplot", 
-         caption  = "")
+         caption  = "Figure 2.3")
+
+head(s.daily_kg_dem_by_trip)
+v.dem_by_trip         <- group_by(s.daily_kg_dem_by_trip, trip)
+s.ave_kg_dem_by_trip  <- summarise(v.dem_by_trip, average_daily_kg = mean(daily_kg), median_daily_kg = median(daily_kg), dif = average_daily_kg - median_daily_kg)
+s.ave_kg_dem_by_trip
+sum(s.ave_kg_dem_by_trip$average_daily_kg)
+sum(s.ave_kg_dem_by_trip$median_daily_kg)
 
 
 
 
-
-
-# 4.1.3 - Daily Kilograms group by trip and service (map) -----------------
-
-summary(base)
-base_f1                     <- filter(base, date >= '2015-12-01')
-dis_by_trip_service         <- group_by(base, trip, service)
-dis_by_trip_service_sum_kgs <- summarise(dis_by_trip_service, total = sum(kilograms), n = n(), promedio = mean(kilograms))
-summary(dis_by_trip_service_sum_kgs)
-sum(dis_by_trip_service_sum_kgs$promedio)
-
-sum(dis_by_trip_service_sum_kgs[dis_by_trip_service_sum_kgs$trip == "MEDIANOS"]$promedio)
-dis_by_trip_service_sum_kgs$trip == "MEDIANOS"
-sum(dis_by_trip_service_sum_kgs[dis_by_trip_service_sum_kgs$trip == "MEDIANOS",]$promedio)
-
-mean(base[base$trip == "MEDIANOS",]$kilograms)
-
-# a)
-qplot(x    = service, 
-      data = base, 
-      geom = "bar",
-      fill = trip,
-      position = "dodge"
-) + labs(title    = "Number of services worked by trip ", 
-         subtitle = "year 2015", 
-         caption  = "")
 
 
 # 4.2 Combined graphs --------------------------------------------------------
